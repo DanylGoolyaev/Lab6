96249533a2e785a5cc5f9e5abe8e4eade8a3fa78
DanylGoolyaev <84317323+DanylGoolyaev@users.noreply.github.com>
2021-11-30 01:09:07 +0200

    Add files via upload

---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FormatDoc;
+            Console.WriteLine("Eneter document format");
+            FormatDoc = Convert.ToString(Console.ReadLine());
+            # throw new NotImplementedException()
+        }
+    }
+
+    abstract class AbstractHandler
+    {
+        FileStream _fileStream;
+        FileInfo _fileInfo;
+        public AbstractHandler(FileStream fileStream, FileInfo fileInfo)
+        {
+            _fileStream = fileStream;
+            _fileInfo = fileInfo;
+        }
+
+        void Open()
+        {
+            _fileStream = _fileInfo.Open(FileMode.Open);
+        }
+
+        void Create()
+        {
+            _fileStream = _fileInfo.Create();
+        }
+
+        void Change(byte[] array)
+        {
+            _fileStream.Write(array, 0, array.Length);
+        }
+
+        void save()
+        {
+           # throw new NotImplementedException()
+        }
+    }
+}
